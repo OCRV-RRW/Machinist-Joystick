f9915becfde90cffbc567060db7d935f9014db11
MomusWinner <areg06.06@mail.ru>
2024-03-01 09:02:05 +0300

    ignore log

--- checker/src/App.js
+++ checker/src/App.js
@@ -1,8 +1,10 @@
 import { HashRouter } from 'react-router-dom';
 import './App.css';
 import { AppRoutes } from './components/Routes';
+import './config.js'
 
 export default function App() {
+
   return (
     <HashRouter >
       <AppRoutes/>
