221590c5451a1a735c9ffddb4a64c044c49e6c09
MomusWinner <areg06.06@mail.ru>
2024-04-01 08:10:59 +0300

    remove routers

--- joystick/src/App.js
+++ joystick/src/App.js
@@ -1,12 +1,11 @@
-import { HashRouter } from 'react-router-dom';
 import './App.css';
 import { AppRoutes } from './components/Routes';
 import './config.js'
 
 export default function App() {
   return (
-    <HashRouter >
-      <AppRoutes/>
-    </HashRouter >
+    <>
+    <AppRoutes/>
+    </>
   );
 }
