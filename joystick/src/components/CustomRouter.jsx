a048d827e5e288f09642e64aa18b95568848ebb7
MomusWinner <areg06.06@mail.ru>
2024-04-01 08:10:43 +0300

    move socket in to CustomRouter

--- joystick/src/components/CustomRouter.jsx
+++ joystick/src/components/CustomRouter.jsx
@@ -1,36 +1,75 @@
 import { Joystick } from './Joystick';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Home } from './Home';
 import { Join } from './Join';
 
+function getSocket(){
+  let socket = new WebSocket('ws://localhost:9000/Joystick') // TODO:
+  console.log("Open socket")
+  socket.addEventListener("message", (e) => {console.log("receive message: " + e.data)})
+  return socket
+}
 
-export function CustomRouter({socket}) {
+var webSocket = getSocket()
+
+export function CustomRouter() {
   const [page, setPage] = useState('/');
+  const [socket, setSocket] = useState(webSocket)
 
   function navigate(url) {
     setPage(url);
   }
 
-  function handleRoomIsCloseEvent(e){
+  const handleRoomIsCloseEvent = useCallback( (e) => {
     var dir = JSON.parse([e.data]);
     if (dir["Type"] !== "RoomIsClosed") return;
     navigate("/")  
-  }
+  })
+
+  const handleSocketClose = useCallback(() =>
+  {
+    console.log("reload")
+    navigate('/reconnection')
+  })
 
   useEffect(() => {
     socket.addEventListener("message", handleRoomIsCloseEvent);
     return () => {
       socket.removeEventListener("message", handleRoomIsCloseEvent);
     };
-  }, []);
+  }, [socket, handleRoomIsCloseEvent]);
+
+  useEffect(() => {
+    console.log("CLOSE------------")
+    socket.addEventListener("close", handleSocketClose);
+    return () => {
+      socket.removeEventListener("close", handleSocketClose);
+    };
+  }, [socket, handleSocketClose]);
 
   let content;
-  if (page === '/') {
+  
+  if (page === '/')
     content = <Home socket={socket} navigate={navigate} />;
-  } else if (page === '/join') {
+  else if (page === '/join')
     content = <Join socket={socket} navigate={navigate}/>;
-  } else if (page === '/joystick') {
+  else if (page === '/joystick')
     content = <Joystick socket={socket} />;
-  }
+  else if (page === '/reconnection')
+    content = <Reconnection setSocket={setSocket} navigate={navigate}/>
+
   return (content);
+}
+
+
+export function Reconnection({setSocket, navigate})
+{
+  function handleReconnect()
+  {
+    console.log(setSocket)
+    setSocket(getSocket())
+    navigate('/')
+  }
+
+  return (<button onClick={handleReconnect}>Переподключиться</button>)
 }
--- joystick/src/components/Routes.jsx
+++ joystick/src/components/Routes.jsx
@@ -1,14 +1,8 @@
 import { CustomRouter } from './CustomRouter';
 
-//var socket = new WebSocket('wss://ocrv-game.ru/Joystick') 
-var socket = new WebSocket('ws://localhost:9000/Joystick') // TODO:
-
-console.log("Open socket")
-socket.addEventListener("message", (e) => {console.log("receive message: " + e.data)})
-
 export function AppRoutes()
 {
     return (
-      <CustomRouter socket={socket}/>
+      <CustomRouter/>
     )
 }
