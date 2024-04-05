6cd77152ff0306c6fc60f43a591cc43d5d67a340
MomusWinner <areg06.06@mail.ru>
2024-04-05 14:12:10 +0300

    disable scalable

--- joystick/src/components/Routes.jsx
+++ joystick/src/components/Routes.jsx
@@ -1,9 +1,14 @@
-import { ChooseRoleAndEnterQueue } from './ChooseRoleAndEnterQueue';
+import { useEffect } from 'react';
 import { CustomRouter } from './CustomRouter';
 
 export function AppRoutes()
 {
-    return (
-      <CustomRouter/>
-    )
+  useEffect(()=>{
+    var viewportmeta = document.querySelector('meta[name="viewport"]');
+    viewportmeta.content = 'user-scalable=NO, width=device-width, initial-scale=1.0'
+  }, [])
+
+  return (
+    <CustomRouter/>
+  )
 }
