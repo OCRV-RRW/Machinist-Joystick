5015f281b6b011b9107ea4cb369dbae2131de5ce
3D23 <dmitrichenkov.dima@mail.ru>
2024-07-10 10:11:18 +0300

    add game end point component

---
+++ joystick/src/components/GameEndPointElement.jsx
@@ -0,0 +1,9 @@
+import './GameEndPointElement.css'
+
+export default function GameEndPointElement({className}) {
+    return (
+        <div className='end-element-container'>
+            <div className={className}></div>
+        </div>
+    )
+}
