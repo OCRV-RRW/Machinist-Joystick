a7051fe536a7313c44cfc9af98e3b2161c8407a5
MomusWinner <areg06.06@mail.ru>
2024-08-21 15:38:53 +0300

    fix bug zoom

--- joystick/src/components/Routes.jsx
+++ joystick/src/components/Routes.jsx
@@ -7,15 +7,30 @@ export function AppRoutes()
 
   useEffect(()=>{
     var viewportmeta = document.querySelector('meta[name="viewport"]');
-    viewportmeta.content = 'user-scalable=NO, width=device-width, initial-scale=1.0'
+    viewportmeta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
     document.ondragstart   = false;
     document.onselectstart = false;
     document.ondragstart = () => false
     window.oncontextmenu = function (event) {
       event.preventDefault();
       event.stopPropagation();
-      return false;
+      return false;      
     };
+
+
+    // отключение zoom через скролл (в том числе трекападами в macOS)
+    document.addEventListener('mousewheel', function(e){
+      if(!e.ctrlKey && !e.metaKey) return;
+
+      e.preventDefault();
+      e.stopImmediatePropagation();
+    }, {passive:false});
+
+    // отключение zoom прикосновениями (в том числе трекападами и т.п.) в Safari и iOS
+    document.addEventListener('gesturestart', function(e){
+      e.preventDefault();
+      e.stopImmediatePropagation();
+    }, {passive:false});
   }, [])
 
   return (
